436a589c7d7bb932f9f494259968ec2f4296b467
alionash <112432218+alionash@users.noreply.github.com>
2023-06-11 01:21:16 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -34,14 +34,14 @@ export default function App() {
 
     return (
         <div
-    //   style={{
-    //     // display: 'flex',
-    //     padding: 20,
-    //     justifyContent: 'center',
-    //     alignItems: 'center',
-    //     fontSize: 40,
-    //     color: '#010101'
-    //   }}
+      style={{
+        // display: 'flex',
+        padding: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
             >
                 <Section title="Please leave feedback">
                 <FeedbackOptions addGood={() => addGood()}
@@ -67,78 +67,3 @@ export default function App() {
 
 
 }
-
-// export class OLdApp extends Component{
-
-//     state = {
-//         good: 0,
-//         neutral: 0,
-//         bad: 0
-//     }
-
-//     addGood = () => {
-//         this.setState(prevState => ({
-//             good: prevState.good + 1,
-//         }))
-//     }
-
-//     addNeutral = () => {
-//         this.setState(prevState => ({
-//             neutral: prevState.neutral + 1,
-//         }))
-//     }
-
-//     addBad = () => {
-//         this.setState(prevState => ({
-//             bad: prevState.bad + 1,
-//         }))
-//     }
-
-    // countTotalFeedback () {
-    //     const { good, neutral, bad } = this.state;
-    //     return good + neutral + bad;
-    // }
-
-//     countPositiveFeedbackPercentage() {
-//         const total = this.countTotalFeedback();
-//             const percentage =  (this.state.good / total) * 100 || 0;
-//             return percentage.toFixed(0)
-//         }
-
-    
-//     render() {
-//         return (
-//         <div
-//       style={{
-//         // display: 'flex',
-//         padding: 20,
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         fontSize: 40,
-//         color: '#010101'
-//       }}
-//             >
-//                 <Section title="Please leave feedback">
-//                 <FeedbackOptions addGood={this.addGood}
-//                     addNeutral={this.addNeutral}
-//                     addBad={this.addBad}
-//                     />
-//                 </Section>
-            
-
-//                 <Section title="Statistics">
-
-//                     {this.countTotalFeedback() === 0 ? (<Notification message="There is no feedback" />) :
-//                         <Statistic
-//                             good={this.state.good}
-//                             neutral={this.state.neutral}
-//                             bad={this.state.bad}
-//                             countTotalFeedback={this.countTotalFeedback()}
-//                             countPositiveFeedbackPercentage={this.countPositiveFeedbackPercentage()} />}
-//                 </Section> 
-//         </div>
-            
-//         )
-//     }
-
-// }
